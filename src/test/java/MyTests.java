f75ae0249a1397e38bbfb1a0011f9a3d7ae7271c
Roman Onyshkiv <roman@Romans-MacBook-Pro-2.local>
2023-07-24 20:11:08 -0400

    standalone added

---
+++ src/test/java/MyTests.java
@@ -0,0 +1,18 @@
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.testng.Assert.assertEquals;
+
+public class MyTests {
+
+    private Response response;
+
+    @Test
+    public void simpleTest() {
+        response = given().contentType("application/json").get("http://localhost:8080/");
+        assertEquals(response.getStatusCode(), 200);
+        assertEquals(response.jsonPath().getInt("id"), 1);
+        assertEquals(response.jsonPath().getInt("properties.age"), 36);
+    }
+}
